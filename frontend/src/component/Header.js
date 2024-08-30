81e308d38f97021f432f6aba2ae0e3f77b894d6f
Shivam <shivam.s@quickwayinfosystems.in>
2024-08-30 12:59:42 +0530

    first commit

---
+++ frontend/src/component/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../assest/logo.png'
+import { Link } from 'react-router-dom'
+
+
+const Header = () => {
+  return (
+    <header className='fixed shadow-md w-full h-16 px-2 md:px-4'>
+        {/* desktop */}
+            <div className='flex items-center h-full'>
+                <Link to="">
+                <div className='h-12'>
+                    <img src={logo} alt='' className='h-full'/>
+                </div>
+                </Link>
+            </div>
+        {/* mobile */}
+    </header>
+  )
+}
+
+export default Header
